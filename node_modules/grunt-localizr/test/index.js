f687895c6cf585e5d32693ee111723c432c3b72a
= <adamjcsmith@gmail.com>
2015-05-04 15:14:20 +0100

    Test 5

--- node_modules/grunt-localizr/test/index.js
+++ node_modules/grunt-localizr/test/index.js
@@ -10,17 +10,15 @@ test('Grunt-localizr', function (t) {
     process.chdir(path.join(process.cwd(), 'test', 'fixtures'));
     grunt.task.init = function() {};
 
-    grunt.initConfig({
-        localizr: {
-            files: ['public/templates/**/*.dust'],
-            options: {
-                contentPath: ['locales/**/*.properties']
-            }
-        }
-    });
-
     t.test('test a localizr build', function(t) {
-
+        grunt.initConfig({
+            localizr: {
+                files: ['public/templates/**/*.dust'],
+                options: {
+                    contentPath: ['locales/**/*.properties']
+                }
+            }
+        });
 
         require('../tasks/index')(grunt);
         grunt.tasks(['localizr'], {}, function(){
@@ -35,6 +33,34 @@ test('Grunt-localizr', function (t) {
             t.equal('<div>Hello</div>', fs.readFileSync('./tmp/US/en/nested/test.dust', 'utf8'));
             t.equal('<div>Test with no Locale </div>', fs.readFileSync('./tmp/US/en/nested/test1.dust', 'utf8'));
             t.equal('<div>Test with no Locale </div>', fs.readFileSync('./tmp/US/en/nested/test1.dust', 'utf8'));
+
+            rimraf('tmp', function() {
+                t.end();
+            });
+
+        });
+    });
+
+    t.test('test a localizr build with alternate dir structure', function(t) {
+        grunt.initConfig({
+            localizr: {
+                files: ['client/templates/**/*.dust'],
+                options: {
+                    contentPath: ['locales/**/*.properties'],
+                    templateRoot: 'client/templates'
+                }
+            }
+        });
+
+        require('../tasks/index')(grunt);
+        grunt.tasks(['localizr'], {}, function(){
+            //verify the files exist
+            t.equal(true, fs.existsSync('./tmp/ES/es/nested/test.dust'));
+            t.equal(true, fs.existsSync('./tmp/US/en/nested/test.dust'));
+
+            //verify they have expected content
+            t.equal('<div>Hola</div>', fs.readFileSync('./tmp/ES/es/nested/test.dust', 'utf8'));
+            t.equal('<div>Hello</div>', fs.readFileSync('./tmp/US/en/nested/test.dust', 'utf8'));
 
             rimraf('tmp', function() {
                 t.end();
